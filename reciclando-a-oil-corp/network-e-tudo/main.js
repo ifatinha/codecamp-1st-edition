c7e741b8ab73a1879d282d8b98f4c163635d0098
Fatima Ferreira <ifatinha99@gmail.com>
2023-09-19 13:27:15 -0300

    Desafio: Network É Tudo!

---
+++ reciclando-a-oil-corp/network-e-tudo/main.js
@@ -0,0 +1,12 @@
+// Vocês decidem que vão enviar projetos e ideias para a Oil Corp seguir sendo mais sustentável. 
+// Uma pessoa do seu grupo diz que vocês receberam um JSON com diversos funcionários da Oil Corp, 
+// e seus emails, e que vocês deveriam tentar enviar as ideias para esses endereços. Porém, 
+// vocês devem poupar esforços e enviar esses emails somente para cargos de liderança e diretoria.
+
+// Crie um código que, faça a analise do JSON procurando por cargos e emails válidos, 
+// e print no console o nome, o cargo e seu email válido.
+
+// Caso não o algoritmo não encontre nenhum cargo e emprego válido, print no console 
+// "Não existem registros encontrados".
+
+// OBS: Emails válidos são aqueles que possuem @oilcorp.com.br completo.
